b4b868145193de8a075debac5eb1f4845ad6ddca
Daniel Green <venix1@gmail.com>
2013-06-02 14:44:24 -0500

    Fixed bugs with build script. Added Fiber support. Removed dmain from libphobos

--- build.sh
+++ build.sh
@@ -27,21 +27,39 @@ pushd /crossdev/gdc-4.8/src
 
 # Extracts archive and converts to git repo. For patch maintenance and rebuilds
 function extract_to_git {
-	nop=""
-}
-
-
-# Utility function handles the clone/reset logic for git sources to avoid 
-# unneccessary downloads.
-function git_clone {
-	nop=""
+	return
+	
+	# Identify archive type
+	# Configure tar arguments
+	# Extract and gitify archive
+	if [ ! -d "$path" ]; then
+		tar $args $1
+		if [ "$2" ]; then
+			mv "$path $2"
+			path=$2
+		fi
+		cd $path
+		git init
+		git config user.email "nobody@localhost"
+		git config user.name "Nobody"
+		git config core.autocrlf false
+		git add *
+		git commit -m "MinGW/GDC restore point"
+		cd ..
+	else
+		if [ "$2" ]; then path=$2; fi
+		cd $path
+		git reset --hard
+		git clean -f	
+		cd ..
+	fi			
 }
 
 # From this point forward, always exit on error
 set -e
 
 # Compile binutils
-if [ ! -e binutils-2.23.2/.built ]; then
+if [ ! -e binutils-2.23.2/build/.built ]; then
 	if [ ! -e "binutils-2.23.2.tar.gz" ]; then
 		wget http://ftp.gnu.org/gnu/binutils/binutils-2.23.2.tar.gz
 	fi
@@ -56,7 +74,7 @@ if [ ! -e binutils-2.23.2/.built ]; then
 		git config user.name "Nobody"
 		git config core.autocrlf false
 		git add *
-		git commit -am "MinGW/GDC restore point"
+		git commit -m "MinGW/GDC restore point"
 		cd ..
 	else
 		cd binutils-2.23.2
@@ -75,7 +93,7 @@ if [ ! -e binutils-2.23.2/.built ]; then
 fi
 
 # Compile Win32 api
-if [ ! -e w32api/.built ]; then
+if [ ! -e w32api/build/.built ]; then
 	if [ ! -e "w32api-3.17-2-mingw32-src.tar.lzma" ]; then
 		wget http://sourceforge.net/projects/mingw/files/MinGW/Base/w32api/w32api-3.17/w32api-3.17-2-mingw32-src.tar.lzma/download
 	fi
@@ -108,7 +126,7 @@ if [ ! -e w32api/.built ]; then
 fi
 
 # Compile MinGW runtime
-if [ ! -e mingwrt-3.20-mingw32/.built ]; then
+if [ ! -e mingwrt-3.20-mingw32/build/.built ]; then
 	if [ ! -e "mingwrt-3.20-mingw32-src.tar.gz" ]; then
 		wget http://sourceforge.net/projects/mingw/files/MinGW/Base/mingw-rt/mingwrt-3.20/mingwrt-3.20-mingw32-src.tar.gz/download
 	fi
@@ -125,7 +143,7 @@ if [ ! -e mingwrt-3.20-mingw32/.built ]; then
 fi
 
 # Compile GMP
-if [ ! -e gmp-4.3.2/.built ]; then
+if [ ! -e gmp-4.3.2/build/.built ]; then
 	if [ ! -e "gmp-4.3.2.tar.bz2" ]; then
 		wget http://ftp.gnu.org/gnu/gmp/gmp-4.3.2.tar.bz2
 	fi
@@ -141,7 +159,7 @@ if [ ! -e gmp-4.3.2/.built ]; then
 fi
 
 # Compile MPFR
-if [ ! -e mpfr-3.1.1/.built ]; then
+if [ ! -e mpfr-3.1.1/build/.built ]; then
 
 	if [ ! -e "mpfr-3.1.1.tar.bz2" ]; then
 		wget http://ftp.gnu.org/gnu/mpfr/mpfr-3.1.1.tar.bz2
@@ -158,7 +176,7 @@ if [ ! -e mpfr-3.1.1/.built ]; then
 fi
 
 # Compile MPC
-if [ ! -e mpc-1.0.1/.built ]; then
+if [ ! -e mpc-1.0.1/build/.built ]; then
 	if [ ! -e "mpc-1.0.1.tar.gz" ]; then
 		wget http://ftp.gnu.org/gnu/mpc/mpc-1.0.1.tar.gz
 	fi
@@ -174,7 +192,7 @@ if [ ! -e mpc-1.0.1/.built ]; then
 fi
 
 # Compile ISL
-if [ ! -e isl-0.11.1/.built ]; then
+if [ ! -e isl-0.11.1/build/.built ]; then
 	if [ ! -e "isl-0.11.1.tar.bz2" ]; then
 		wget ftp://gcc.gnu.org/pub/gcc/infrastructure/isl-0.11.1.tar.bz2
 	fi
@@ -190,7 +208,7 @@ if [ ! -e isl-0.11.1/.built ]; then
 fi
 
 # Compile CLOOG
-if [ ! -e cloog-0.18.0/.built ]; then
+if [ ! -e cloog-0.18.0/build/.built ]; then
 	if [ ! -e "cloog-0.18.0.tar.gz" ]; then
 	wget ftp://gcc.gnu.org/pub/gcc/infrastructure/cloog-0.18.0.tar.gz
 	fi
@@ -242,6 +260,7 @@ function build_gdc {
 	
 	pushd GDC
 	patch -p1 < $root/patches/mingw-gdc.patch
+	patch -p1 < $root/patches/no_dmain.patch
 	./setup-gcc.sh ../gcc-4.8.0
 	popd
 
@@ -253,8 +272,8 @@ function build_gdc {
 	cd build
 	
 	# Must build GCC using patched mingwrt
-	export LPATH="$(GCC_PREFIX)/lib;$(GCC_PREFIX)/i686-pc-mingw32/lib"
-	export CPATH="$(GCC_PREFIX)/include"
+	export LPATH="$GCC_PREFIX/lib;$GCC_PREFIX/i686-pc-mingw32/lib"
+	export CPATH="$GCC_PREFIX/include"
 
 	../configure --prefix=$GCC_PREFIX --build=i686-mingw32 --with-gmp=/crossdev/gdc-4.8/gmp-4.3.2 --with-mpfr=/crossdev/gdc-4.8/mpfr-3.1.1 --with-mpc=/crossdev/gdc-4.8/mpc-1.0.1 --with-cloog=/crossdev/gdc-4.8/cloog-0.18.0 --with-isl=/crossdev/gdc-4.8/isl-0.11.1 --disable-bootstrap --enable-languages=c,c++,d,lto --enable-sjlj-exceptions 
 	make && make install
